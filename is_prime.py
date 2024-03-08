950aa7cd3ac3d0b5698ed61fdfc75c4aa919fa82
Rediet Deneke <redietd1@umd.edu>
2024-03-07 20:45:37 -0600

    feat: is_prime

---
+++ is_prime.py
@@ -0,0 +1,11 @@
+def is_prime(num):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+    return True
+
+for i in range(1, 20):
+	if is_prime(i + 1):
+			print(i + 1, end=" ")
+print()
